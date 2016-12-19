2efc40a7e9c288d6b7034a02dded7727ee42355b
Jonathan Darling <jmdarling@me.com>
2016-12-19 12:22:43 -0600

    Fixing environment variable name.

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ module.exports = {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/justlogmebaby',
   port: process.env.PORT || 8080,
   redisUrl: process.env.REDIS_URL || '//localhost:6379',
-  stdoutFileDestination: process.env.JLMB_STDOUT_FILE_DESTINATION || null,
-  stderrFileDestination: process.env.JLMB_STDERR_FILE_DESTINATION || null
+  stdoutFileDestination: process.env.JLMB_MONGO_WORKER_STDOUT_FILE_DESTINATION || null,
+  stderrFileDestination: process.env.JLMB_MONGO_WORKER_STDERR_FILE_DESTINATION || null
 }
